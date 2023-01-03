4e6f7f6ce07471139dee63f7d7fcacdaabb10f41
rodycoco <enjoyrodycoco@gmail.com>
2023-01-03 16:35:12 +0800

    update the way for save image

--- evaluate_rl_tsne.py
+++ evaluate_rl_tsne.py
@@ -25,9 +25,10 @@ data_label = np.array([1,1,2,2,3,3,4,4,5,5])
 agent = DiagonalGaussianPolicy(N=data.shape[0], low_dim=my_args.n_components, \
     high_dim=data.shape[1], lr=my_args.lr, device_id=my_args.device_id, hidden_dim=my_args.hidden_dim)
 agent.policy.load_state_dict(torch.load("model.pkl"))
+video_per_frame = 10
 
 if __name__ == "__main__":
-    videowrite = cv2.VideoWriter('plots/video/result.mp4',cv2.VideoWriter_fourcc(*'mp4v'),20,(640,480))
+    videowrite = cv2.VideoWriter('plots/video/result.mp4',cv2.VideoWriter_fourcc(*'mp4v'),video_per_frame,(640,480))
     init_mean = np.zeros(my_args.n_components)
     init_cov = np.identity(my_args.n_components) * 1e-4
     Y = np.random.multivariate_normal(mean=init_mean, cov=init_cov, size=data.shape[0])
@@ -40,7 +41,11 @@ if __name__ == "__main__":
     cur_time = str(datetime.now())[:-7]
     writer = SummaryWriter(log_dir="evaluating_logs/"+cur_time)
     
-    for t in range(200):
+    save_tsne_result(Y.detach().cpu().numpy(), data_label, "plots/figure/t.png")
+    img = cv2.imread("plots/figure/t.png")
+    videowrite.write(img)
+
+    for t in range(my_args.steps):
         low_dim_data = Y.reshape(1, -1)
         high_dim_data = data.reshape(1, -1)
         s_t = torch.cat((low_dim_data, high_dim_data), dim=1).cuda(my_args.device_id)
