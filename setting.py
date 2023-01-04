3f65d8657ecd0482e7defe87a4bc71903bb7dbb9
rodycoco <enjoyrodycoco@gmail.com>
2023-01-05 00:35:07 +0800

    digit dataset

--- setting.py
+++ setting.py
@@ -1,12 +1,12 @@
 class my_args():
     n_components = 2
-    steps = 50
-    episodes = 200
+    steps = 100
+    episodes = 1000
     device_id = 7
     perplexity = 2
-    lr = 2e-3
+    lr = 1e-4
     env_num = 64
     gamma = 0.5
-    hidden_dim = [20,10,5]
+    hidden_dim = [200,100,50]
     seed = 0
     perp_tol=1e-8
